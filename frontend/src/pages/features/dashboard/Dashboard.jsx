9b1f48f8b0f03e2bc5aeae4b181c3a3cd12bc6eb
rajendraxD <rajendraxd1@gmail.com>
2026-07-24 20:39:52 +0530

    feat: add logout button to dashboard

--- frontend/src/pages/features/dashboard/Dashboard.jsx
+++ frontend/src/pages/features/dashboard/Dashboard.jsx
@@ -1,5 +1,22 @@
+import Button from "@mui/material/Button";
+import { useDispatch } from "react-redux";
+import { logout } from "../../../store/slices/userSlice";
+
 export default function Dashboard() {
+  const dispatch = useDispatch();
+  // const { logout } = useSelector((state) => state.user);
+
+  const logoutClick = async (e) => {
+    e.preventDefault();
+    try {
+      await dispatch(logout());
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
-    <div>Dashboard</div>
+    <div className="flex justify-center items-center">
+      <Button onClick={logoutClick}>Logout</Button>
+    </div>
   )
 }
